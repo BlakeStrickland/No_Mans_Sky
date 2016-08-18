18df4a53b0936dba70dc3f095c83292a85d8bb48
Blake Strickland <Stricklandb02@gmail.com>
2016-08-17 23:43:07 -0400

    Today and all time readings, route to dashboard, total methods and better page layout

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,4 +1,8 @@
 class DashboardsController < ApplicationController
   def home
+    @distances = Distance.order(:created_at)
+    @todays = @distances.where("created_at >= ?", Time.zone.now.beginning_of_day)
+    @total = @distances.total_light_years_traveled()
+    @todays_total = @todays.total_light_years_traveled()
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :distances
+  root 'dashboards#home'
   get 'dashboards/home'
 
   # The priority is based upon order of creation: first created -> highest priority.
