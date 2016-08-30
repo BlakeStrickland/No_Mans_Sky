5137ccc89cfc7808f661bf5ea0d25e5a171b8e67
Blake Strickland <Stricklandb02@gmail.com>
2016-08-30 17:56:56 -0400

    Capture everything in dashboard controller and show on dashboard home

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,18 +1,13 @@
 class DashboardsController < ApplicationController
   def home
-    # @distances = Distance.order(:created_at)
-    # @todays = @distances.where("created_at >= ?", Time.zone.now.beginning_of_day)
-    # @total = @distances.total_light_years_traveled()
-    # @todays_total = @todays.total_light_years_traveled()
-
-    @alloys = Alloy.where(@current_user == User.id)
-    @elements = Alloy.where(@current_user == User.id)
-    @energys = Energy.where(@current_user == User.id)
-    @exo_upgrades = Alloy.where(@current_user == User.id)
-    @mulit_upgrades = Alloy.where(@current_user == User.id)
-    @ship_upgrades = Alloy.where(@current_user == User.id)
-    @technologies = Alloy.where(@current_user == User.id)
-    @trade_commodities = Alloy.where(@current_user == User.id)
+    @alloys = Alloy.all
+    @elements = Element.all
+    @energys = Energy.all
+    @exo_upgrades = ExosuitUpgrade.all
+    @multitool_upgrades = MultitoolUpgrade.all
+    @ship_upgrades = ShipUpgrade.all
+    @technologies = Technology.all
+    @trade_commodities = TradeCommodity.all
 
   end
 end
