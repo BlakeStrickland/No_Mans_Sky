638daaac29ef2035af06140bd2bf9c65f049d5ce
Blake Strickland <Stricklandb02@gmail.com>
2016-09-01 16:46:58 -0400

    Connect relations

--- app/models/exosuit.rb
+++ app/models/exosuit.rb
@@ -1,4 +1,4 @@
 class Exosuit < ActiveRecord::Base
   belongs_to :user
-
+  has_many :exosuit_upgrades
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,4 +1,5 @@
 class Ship < ActiveRecord::Base
   belongs_to :user
-  
+  has_many :ship_upgrades
+
 end
